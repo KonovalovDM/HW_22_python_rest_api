6a12aeb283de9e3147b9ac87daa97965a1d77c85
KonovalovDM <kdm232006@gmail.com>
2025-10-26 22:18:57 +0300

    add some code

--- eshop/business_logic/product_usecases.py
+++ eshop/business_logic/product_usecases.py
@@ -1,15 +1,23 @@
 from typing import Optional, List
+import uuid
 
 from eshop.business_logic.product import Product
+from eshop.data_access import product_repo
 
 
 def product_create(dto) -> Product:
-    raise Exception('Not implemented yet')
+    product = Product(
+        id=str(uuid.uuid4()),
+        name=dto["name"],
+        price=dto["price"]
+    )
+    product_repo.save(product)
+    return product
 
 
 def product_get_by_id(id: str) -> Optional[Product]:
-    raise Exception('Not implemented yet')
+    return product_repo.get_by_id(id)
 
 
 def product_get_many(page: int, limit: int) -> List[Product]:
-    raise Exception('Not implemented yet')
+    return product_repo.get_many(page, limit)
--- eshop/view/server.py
+++ eshop/view/server.py
@@ -52,6 +52,52 @@ def order_get_by_id_endpoint(id):
 
     return OrderSchema().dump(order)
 
+from eshop.business_logic.product_usecases import (
+    product_create,
+    product_get_by_id,
+    product_get_many,
+)
+from eshop.view.product_schemas import (
+    ProductCreateDtoSchema,
+    ProductSchema,
+    ProductGetManyParams,
+)
+
+
+@app.post("/api/v1/product")
+def product_create_endpoint():
+    try:
+        product_create_dto = ProductCreateDtoSchema().load(request.json)
+    except ValidationError as err:
+        return err.messages, 400
+
+    product = product_create(product_create_dto)
+    return ProductSchema().dump(product)
+
+
+@app.get("/api/v1/product")
+def product_get_many_endpoint():
+    try:
+        product_get_many_params = ProductGetManyParams().load(request.args)
+    except ValidationError as err:
+        return err.messages, 400
+
+    products = product_get_many(
+        page=product_get_many_params["page"],
+        limit=product_get_many_params["limit"],
+    )
+
+    return ProductSchema(many=True).dump(products)
+
+
+@app.get("/api/v1/product/<id>")
+def product_get_by_id_endpoint(id):
+    product = product_get_by_id(id)
+    if product is None:
+        return {"error": "Not found"}, 404
+
+    return ProductSchema().dump(product)
+
 
 def run_server():
     app.run()
